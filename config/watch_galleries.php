43c77d28820e3faf91fa53a3e06ed1d6832429e2
v4800 <157183990+v4800@users.noreply.github.com>
2026-09-07 02:50:33 +0200

    Add configurable product image galleries

---
+++ config/watch_galleries.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    45 => [
+        '/images/watches/classique-bleu-roi-vvs/front-three-quarter.webp',
+        '/images/watches/classique-bleu-roi-vvs/front.webp',
+        '/images/watches/classique-bleu-roi-vvs/side.webp',
+        '/images/watches/classique-bleu-roi-vvs/back.webp',
+    ],
+];
