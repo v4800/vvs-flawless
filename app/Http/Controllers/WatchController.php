8a19882935135f2ac2ce4872663d130264a4149d
v4800 <157183990+v4800@users.noreply.github.com>
2026-09-08 02:39:24 +0200

    Use approved blue watch image in collection

--- app/Http/Controllers/WatchController.php
+++ app/Http/Controllers/WatchController.php
@@ -15,6 +15,19 @@ class WatchController extends Controller
 
         $watches = Watch::latest()->get();
 
+        $watches->transform(function (Watch $watch): Watch {
+            if (
+                Str::contains(
+                    (string) $watch->image,
+                    'presidentielle-bleu-romains'
+                )
+            ) {
+                $watch->image = '/images/watches/catalog/001-blue-round/02-hero-reflection.png';
+            }
+
+            return $watch;
+        });
+
         return inertia('Watches/Index', [
             'watches' => $watches,
 
