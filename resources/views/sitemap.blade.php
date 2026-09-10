095ea6fb71d757ff31602a6a08c2279009be7380
v4800 <157183990+v4800@users.noreply.github.com>
2026-09-11 01:05:50 +0200

    Keep sitemap XML output valid

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -32,7 +32,6 @@
         ],
     ];
 @endphp
-<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     @foreach ($staticGroups as $group)
         @foreach ($group as $currentUrl)
