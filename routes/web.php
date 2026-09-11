9c24b7c0ebbcca90e581c2bfead3fe085b6b77fa
v4800 <157183990+v4800@users.noreply.github.com>
2026-09-11 17:31:15 +0200

    Add SSR catalog product routes

--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,9 @@ Route::get(
 Route::get('/watches', [WatchController::class, 'index'])
     ->name('watches.index');
 
+Route::get('/watches/catalog/{catalogSlug}', [WatchController::class, 'showCatalog'])
+    ->name('catalog.show');
+
 Route::get('/watches/{watchId}', [WatchController::class, 'redirectLegacy'])
     ->whereNumber('watchId')
     ->name('watches.legacy');
@@ -140,6 +143,9 @@ Route::prefix('nl')
 
         Route::get('/watches', [WatchController::class, 'index'])
             ->name('watches.index');
+
+        Route::get('/watches/catalog/{catalogSlug}', [WatchController::class, 'showCatalog'])
+            ->name('catalog.show');
 
         Route::get('/watches/{watchId}', [WatchController::class, 'redirectLegacy'])
             ->whereNumber('watchId')
@@ -215,6 +221,9 @@ Route::prefix('en')
 
         Route::get('/watches', [WatchController::class, 'index'])
             ->name('watches.index');
+
+        Route::get('/watches/catalog/{catalogSlug}', [WatchController::class, 'showCatalog'])
+            ->name('catalog.show');
 
         Route::get('/watches/{watchId}', [WatchController::class, 'redirectLegacy'])
             ->whereNumber('watchId')
