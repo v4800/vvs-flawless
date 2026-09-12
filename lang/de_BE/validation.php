a2b2e146fc6db9e1a112fef5c1fd08018dd17b9a
v4800 <157183990+v4800@users.noreply.github.com>
2026-09-12 23:53:53 +0200

    Add German reservation validation messages

---
+++ lang/de_BE/validation.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'accepted' => 'Das Feld :attribute muss akzeptiert werden.',
+    'email' => 'Das Feld :attribute muss eine gültige E-Mail-Adresse enthalten.',
+    'exists' => 'Die Auswahl für :attribute ist ungültig.',
+    'in' => 'Die Auswahl für :attribute ist ungültig.',
+    'integer' => 'Das Feld :attribute muss eine ganze Zahl sein.',
+    'max' => [
+        'string' => 'Das Feld :attribute darf höchstens :max Zeichen enthalten.',
+    ],
+    'required' => 'Das Feld :attribute ist erforderlich.',
+    'string' => 'Das Feld :attribute muss Text enthalten.',
+
+    'attributes' => [
+        'watch_id' => 'Uhr',
+        'movement' => 'Werk',
+        'customer_name' => 'vollständiger Name',
+        'email' => 'E-Mail-Adresse',
+        'phone' => 'Telefonnummer',
+        'city' => 'Stadt',
+        'delivery_method' => 'Empfangsart',
+        'message' => 'Nachricht',
+        'confirmation' => 'Bestätigung',
+    ],
+];
