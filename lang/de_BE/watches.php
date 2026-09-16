ab3678e2b7c887149cc8cfb9654bdecd680e63fd
v4800 <157183990+v4800@users.noreply.github.com>
2026-09-17 00:29:30 +0200

    Localize watch descriptions and SEO copy

--- lang/de_BE/watches.php
+++ lang/de_BE/watches.php
@@ -1,6 +1,62 @@
 <?php
 
 return [
+    'presented' => [
+        'name' => 'Quadratisch · Weißes Zifferblatt, schwarzes Lederband',
+        'short_description' => 'Quadratisches besetztes Gehäuse, römische Ziffern und schwarzes Lederband.',
+        'description' => 'Quadratisches Gehäuse mit Moissanit, römische Ziffern und schwarzes Lederband. Angegebenes Gesamtsteingewicht: 20–30 ct, für dieses Exemplar zu bestätigen.',
+        'ui' => [
+            'model_label' => 'Gezeigtes Modell',
+            'price_label' => 'Preis des gezeigten Modells',
+            'stone_weight_label' => 'Angegebenes Gesamtsteingewicht',
+            'stone_weight_value' => '20–30 ct, für dieses Exemplar zu bestätigen',
+            'confirm_title' => 'Vor der Bestellung zu bestätigen',
+            'confirm_details' => 'Steingewicht dieses Exemplars, Abmessungen und Referenz des Uhrwerks.',
+            'visual_note' => 'Der Fotohintergrund wurde bearbeitet. Bitte vor der Bestätigung ein Video des angebotenen Exemplars anfordern.',
+        ],
+    ],
+    'catalog' => [
+        'twotone-round' => [
+            'name' => '41 mm · Bicolor, Champagner-Zifferblatt',
+            'short_description' => 'Bicolor-Ausführung, Champagner-Zifferblatt und integriertes Armband.',
+            'description' => 'Eine 41-mm-Uhr mit Bicolor-Ausführung, Champagner-Zifferblatt und integriertem Armband, besetzt mit VVS-Moissanit in Farbe D.',
+        ],
+        'black-square' => [
+            'name' => '41 mm · Schwarz eckig, römische Ziffern',
+            'short_description' => 'Schwarzes eckiges Gehäuse, römische Ziffern und VVS-Besatz.',
+            'description' => 'Eine schwarze eckige 41-mm-Uhr mit römischen Ziffern, besetzt mit VVS-Moissanit in Farbe D.',
+        ],
+        'sport-black-strap' => [
+            'name' => '41 mm · Chronograph, schwarzes Armband',
+            'short_description' => 'Chronograph, schwarzes Armband und Pavé-Zifferblatt.',
+            'description' => '41-mm-Chronograph mit schwarzem Armband, vollständig besetztem Gehäuse und Pavé-Zifferblatt aus VVS-Moissanit in Farbe D. Schwarze Drücker verstärken den sportlichen Kontrast.',
+        ],
+        'gold-black-daydate' => [
+            'name' => '41 mm · Gelbgold, schwarzes Zifferblatt',
+            'short_description' => 'Gelbgoldfarbene Ausführung, schwarzes Zifferblatt und kontrastierendes Armband.',
+            'description' => 'Gelbgoldfarbene Ausführung, schwarzes Zifferblatt und VVS-Moissanit in Farbe D. Das Armband behält polierte Mittelglieder und besetzte Außenglieder für einen klaren Kontrast.',
+        ],
+        'rose-skeleton' => [
+            'name' => '41 mm · Bicolor Skelett, Roségold',
+            'short_description' => 'Skelett-Zifferblatt mit roségoldfarbenen Akzenten.',
+            'description' => 'Skelett-Zifferblatt, stahlfarbene Ausführung mit roségoldfarbenen Akzenten und VVS-Moissanit in Farbe D. Das integrierte Armband greift die Bicolor-Details des Gehäuses auf.',
+        ],
+        'square-bicolor-paved' => [
+            'name' => '41 mm · Bicolor eckig, Pavé-Zifferblatt',
+            'short_description' => 'Eckiges Gehäuse, Pavé-Zifferblatt und Bicolor-Ausführung.',
+            'description' => 'Eckiges Gehäuse, Pavé-Zifferblatt mit römischen Ziffern und Bicolor-Ausführung mit gelbgoldfarbenen Akzenten. Der Besatz aus VVS-Moissanit in Farbe D setzt sich auf dem integrierten Armband fort.',
+        ],
+        'women-pink' => [
+            'name' => '36 mm · Damenuhr, rosa Zifferblatt',
+            'short_description' => 'Rosa Zifferblatt, Pavé-Armband und 36-mm-Format.',
+            'description' => '36-mm-Damenmodell mit rosa Zifferblatt, vollständig pavébesetztem Armband und VVS-Moissanit in Farbe D.',
+        ],
+        'women-square-36' => [
+            'name' => '36 mm · Eckige Damenuhr, weißes Zifferblatt',
+            'short_description' => 'Eckiges Gehäuse, weißes Zifferblatt und Pavé-Armband.',
+            'description' => 'Eckiges 36-mm-Damenmodell mit weißem Zifferblatt und römischen Ziffern, Pavé-Armband und VVS-Moissanit in Farbe D.',
+        ],
+    ],
     42 => [
         'name' => '41 mm · Camouflage-Chronograph',
         'short_description' => 'Camouflage-Zifferblatt, sportlicher Stil.',
